ce615d5f60a33e8dad54fd209b57b3878325b404
letrunghieu07 <trunghieu07032003@gmail.com>
2024-10-05 01:35:53 +0700

    update sale

--- src/admin/component/product/ModalEdit.jsx
+++ src/admin/component/product/ModalEdit.jsx
@@ -1,213 +1,52 @@
-// import { Modal } from "antd";
-// import { FaEdit } from "react-icons/fa";
-// import { useState,useEffect } from "react";
-
-// const ModalEdit = ({ isOpen, handleClose, title, handleSubmit, mausac }) => {
-//   const [newColorName, setNewColorName] = useState("");
-
-//   const handleConfirmEdit = () => {
-//     handleSubmit(mausac?.id, { tenMau: newColorName });
-//   };
-
-//   useEffect(() => {
-//     if (mausac) {
-//       setNewColorName(mausac.tenMau); 
-//     }
-//   }, [mausac]);
-
-//   return (
-//     <>
-//       <Modal
-//         open={isOpen}
-//         title={
-//           <span className="flex">
-//             <FaEdit
-//               style={{ color: "green", marginRight: 8, fontSize: "1.5rem" }}
-//             />
-//             Chỉnh sửa {title}
-//           </span>
-//         }
-//         okType="primary"
-//         onOk={handleConfirmEdit}
-//         onCancel={handleClose}
-//         footer={(_, { OkBtn, CancelBtn }) => (
-//           <>
-//             <CancelBtn />
-//             <OkBtn />
-//           </>
-//         )}
-//         keyboard={false}
-//         maskClosable={false}
-//       >
-//         <input
-//           onChange={(e) => setNewColorName(e.target.value)}
-//           value={newColorName}
-//           className="w-full border rounded-sm h-8 p-4"
-//         ></input>
-//       </Modal>
-//     </>
-//   );
-// };
-// export default ModalEdit;
-
-
 import { Modal } from "antd";
 import { FaEdit } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 
-const ModalEdit = ({ isOpen, handleClose, title, handleSubmit, voucher }) => {
-  const [newVoucherName, setNewVoucherName] = useState("");
-  const [maVoucher, setMaVoucher] = useState("");
-  const [giaTriGiam, setGiaTriGiam] = useState("");
-  const [giaTriDonHangToiThieu, setGiaTriDonHangToiThieu] = useState("");
-  const [giaTriGiamToiDa, setGiaTriGiamToiDa] = useState(""); // Thêm trường này
-  const [ngayBatDau, setNgayBatDau] = useState("");
-  const [ngayKetThuc, setNgayKetThuc] = useState("");
-  const [hinhThucGiam, setHinhThucGiam] = useState(""); 
-  const [soLuong, setSoLuong] = useState(""); 
-  const [trangThai, setTrangThai] = useState(""); 
+const ModalEdit = ({ isOpen, handleClose, title, handleSubmit, mausac }) => {
+  const [newColorName, setNewColorName] = useState("");
 
   const handleConfirmEdit = () => {
-    handleSubmit(voucher?.id, {
-      tenVoucher: newVoucherName,
-      maVoucher,
-      giaTriGiam: parseFloat(giaTriGiam),
-      giaTriDonHangToiThieu: parseFloat(giaTriDonHangToiThieu),
-      giaTriGiamToiDa: parseFloat(giaTriGiamToiDa), // Gửi giá trị này
-      ngayBatDau,
-      ngayKetThuc,
-      hinhThucGiam,
-      soLuong: parseInt(soLuong, 10),
-      trangThai,
-    });
+    handleSubmit(mausac?.id, { tenMau: newColorName });
   };
 
   useEffect(() => {
-    if (voucher) {
-      setNewVoucherName(voucher.tenVoucher);
-      setMaVoucher(voucher.maVoucher);
-      setGiaTriGiam(voucher.giaTriGiam);
-      setGiaTriDonHangToiThieu(voucher.giaTriDonHangToiThieu);
-      setGiaTriGiamToiDa(voucher.giaTriGiamToiDa); // Khởi tạo giá trị cho trường này
-      setNgayBatDau(voucher.ngayBatDau);
-      setNgayKetThuc(voucher.ngayKetThuc);
-      setHinhThucGiam(voucher.hinhThucGiam);
-      setSoLuong(voucher.soLuong);
-      setTrangThai(voucher.trangThai);
+    if (mausac) {
+      setNewColorName(mausac.tenMau); 
     }
-  }, [voucher]);
+  }, [mausac]);
 
   return (
-    <Modal
-      open={isOpen}
-      title={
-        <span className="flex">
-          <FaEdit
-            style={{ color: "green", marginRight: 8, fontSize: "1.5rem" }}
-          />
-          Chỉnh sửa {title}
-        </span>
-      }
-      okType="primary"
-      onOk={handleConfirmEdit}
-      onCancel={handleClose}
-      footer={(_, { OkBtn, CancelBtn }) => (
-        <>
-          <CancelBtn />
-          <OkBtn />
-        </>
-      )}
-      keyboard={false}
-      maskClosable={false}
-    >
-      <div className="mb-4">
-        <label>Tên voucher</label>
+    <>
+      <Modal
+        open={isOpen}
+        title={
+          <span className="flex">
+            <FaEdit
+              style={{ color: "green", marginRight: 8, fontSize: "1.5rem" }}
+            />
+            Chỉnh sửa {title}
+          </span>
+        }
+        okType="primary"
+        onOk={handleConfirmEdit}
+        onCancel={handleClose}
+        footer={(_, { OkBtn, CancelBtn }) => (
+          <>
+            <CancelBtn />
+            <OkBtn />
+          </>
+        )}
+        keyboard={false}
+        maskClosable={false}
+      >
         <input
-          onChange={(e) => setNewVoucherName(e.target.value)}
-          value={newVoucherName}
+          onChange={(e) => setNewColorName(e.target.value)}
+          value={newColorName}
           className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Mã voucher</label>
-        <input
-          onChange={(e) => setMaVoucher(e.target.value)}
-          value={maVoucher}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Giá trị giảm</label>
-        <input
-          type="number"
-          onChange={(e) => setGiaTriGiam(e.target.value)}
-          value={giaTriGiam}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Giá trị đơn hàng tối thiểu</label>
-        <input
-          type="number"
-          onChange={(e) => setGiaTriDonHangToiThieu(e.target.value)}
-          value={giaTriDonHangToiThieu}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Giá trị giảm tối đa</label>
-        <input
-          type="number"
-          onChange={(e) => setGiaTriGiamToiDa(e.target.value)} // Xử lý thay đổi
-          value={giaTriGiamToiDa} // Liên kết với giá trị
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Ngày bắt đầu</label>
-        <input
-          type="datetime-local"
-          onChange={(e) => setNgayBatDau(e.target.value)}
-          value={ngayBatDau}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Ngày kết thúc</label>
-        <input
-          type="datetime-local"
-          onChange={(e) => setNgayKetThuc(e.target.value)}
-          value={ngayKetThuc}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Hình thức giảm</label>
-        <input
-          onChange={(e) => setHinhThucGiam(e.target.value)}
-          value={hinhThucGiam}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Số lượng</label>
-        <input
-          type="number"
-          onChange={(e) => setSoLuong(e.target.value)}
-          value={soLuong}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-      <div className="mb-4">
-        <label>Trạng thái</label>
-        <input
-          onChange={(e) => setTrangThai(e.target.value)}
-          value={trangThai}
-          className="w-full border rounded-sm h-8 p-4"
-        />
-      </div>
-    </Modal>
+        ></input>
+      </Modal>
+    </>
   );
 };
-
 export default ModalEdit;
+
